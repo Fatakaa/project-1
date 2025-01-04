b0951e21c5b824200572bcd8ffdbd746e3ec4db5
Fatakaa <dwikja1@gmail.com>
2025-01-04 19:38:18 +0800

    Initial commit

---
+++ app/Filament/Resources/PengajuanResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PengajuanResource\Pages;
+use App\Filament\Resources\PengajuanResource\RelationManagers;
+use App\Models\Pengajuan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PengajuanResource extends Resource
+{
+    protected static ?string $model = Pengajuan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nim'),
+                TextInput::make('nama_mahasiswa'),
+                TextInput::make('fakultas'),
+                TextInput::make('prodi'),
+                TextInput::make('nama_surat')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nim'),
+                TextColumn::make('nama_mahasiswa'),
+                TextColumn::make('fakultas'),
+                TextColumn::make('prodi'),
+                TextColumn::make('nama_surat')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPengajuans::route('/'),
+            'create' => Pages\CreatePengajuan::route('/create'),
+            'edit' => Pages\EditPengajuan::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2025_01_04_104301_create_pengajuans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengajuans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim');
+            $table->string('nama_mahasiswa');
+            $table->string('fakultas');
+            $table->string('prodi');
+            $table->string('nama_surat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengajuans');
+    }
+};
